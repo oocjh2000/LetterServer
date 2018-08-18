638770a0bc7989413218ca788c4d2bf7238489ec
DESKTOP-27VVUBG\chats <oocjh200@gmail.com>
2018-08-18 19:51:52 +0900

    서버끝?

--- LetterServer_/server.js
+++ LetterServer_/server.js
@@ -1,8 +1,31 @@
 'use strict';
 var http = require('http');
 var port = process.env.PORT || 1337;
+var express = require('express');
+var app = express();
+const bodyParser = require('body-parser');
 
-http.createServer(function (req, res) {
-    res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.end('Hello World\n');
-}).listen(port);
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+
+var Message = 'null';
+var accept = false;
+app.listen(12358, () => {
+    console.log('router listening');
+});
+app.get('/Letter', (req, res) => {
+    res.status(200).json(Message);
+});
+app.get('/accept', (req, res) => {
+    res.status(200).json(accept);
+});
+app.put('/', (req, res) => {
+    Message = req.body.Message;
+    res.status(200).json(Message);
+});
+app.put('/:acc', (req, res) => {
+    var mind = req.params.acc;
+    accept = mind;
+    res.status(200).json(accept);
+});
